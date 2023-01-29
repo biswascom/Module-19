822f065da4ee1863cc3bfaa33a441fbce1ebaa1a
biswascom <abjessbd@gmail.com>
2023-01-29 21:32:43 +0600

    First commit

---
+++ while-for-loop.js
@@ -0,0 +1,72 @@
+// var marks = [12, 23, 65, 77, 56, 88, 99];
+// var playersName = ['Shakib', 'Riyad', 'Mushfique', 'Mahmudullah', 'Mashrafe'];
+
+// console.log (playersName.length);
+// console.log (playersName.indexOf('Riyad'));
+// playersName.pop();
+// playersName.push('Rubel Hossain');
+// playersName[0] = 'Al-Amin Biswas';
+// var popularPlayer = playersName[1];
+// console.log (popularPlayer);
+
+// var i = 0;
+// while (i < marks.length) {
+//     var mark = marks[i];
+//     console.log (mark);
+//     i++;
+// }
+
+// for (var i = 0; i < marks.length; i++) {
+//     var mark = marks[i];
+//     console.log (mark);
+// }
+
+// var i = 0;
+// while (i < playersName.length) {
+//     var player = playersName[i];
+//     console.log (player);
+//     i++;
+// }
+
+// for (var i = 0; i < playersName.length; i++) {
+//     var player = playersName[i];
+//     console.log (player);
+// }
+
+// var i = playersName.length;
+// while (i >= 0) {
+//     var player = playersName[i];
+//     console.log (player);
+//     i--;
+// }
+
+// for (i = playersName.length; i >= 0; i--) {
+//     var player = playersName[i];
+//     console.log (player);
+// }
+
+var myDailyWorks = ['১) রাত ৮ টা বাজে মডিউল আনলক করো ', '২) ফটাফট ভিডিও দেখে দেখে প্রাকটিস করো', '৩) ভিডিও দেখতে দেখতে নোটস নাও ', '৪) মডিউল শেষ হলে পুরা মডিউল নিজে নিজে প্রাকটিস করো ', '৫) কোন কিছু বুঝতে না পারলে (চিন্তা করে দেখো এইখানে কিন্তু একটা শর্ত আছে ), সাপোর্ট সেশনে জয়েন করো '];
+
+// for (i = 0; i < myDailyWorks.length; i++) {
+//     var work = myDailyWorks[i];
+//     console.log (work);
+// }
+
+// for (i = myDailyWorks.length; i >= 0; i--) {
+//     var work = myDailyWorks[i];
+//     console.log (work);
+// }
+
+// var i = 0;
+// while (i < myDailyWorks.length) {
+//     var work = myDailyWorks[i];
+//     console.log (work);
+//     i++;
+// }
+
+var i = myDailyWorks.length;
+while (i >= 0) {
+    var work = myDailyWorks[i];
+    console.log (work);
+    i--;
+}
